78ca4f71531ce8dded131401cb4ac1e0eba2431d
learntopilk <jonsterivilja@gmail.com>
2018-05-23 21:31:49 +0300

    Tehtävä 2.4 tehty
    Kursseja voi nyt olla mielivaltainen määrä.

--- teht2_kurssiensisalto/src/index.js
+++ teht2_kurssiensisalto/src/index.js
@@ -46,30 +46,51 @@ const Osa = ({osa}) => {
 }
 
 const App = () => {
-    const kurssi = {
+    const kurssit = [
+      {
         nimi: 'Half Stack -sovelluskehitys',
+        id: 1,
         osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10,
-                id: 1
-            },
-            {
-                nimi: 'Tiedonvälitys propseilla',
-                tehtavia: 7,
-                id: 2
-            },
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14,
-                id: 3
-            }
+          {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10,
+            id: 1
+          },
+          {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7,
+            id: 2
+          },
+          {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14,
+            id: 3
+          }
         ]
-    }
+      },
+      {
+        nimi: 'Node.js',
+        id: 2,
+        osat: [
+          {
+            nimi: 'Routing',
+            tehtavia: 3,
+            id: 1
+          },
+          {
+            nimi: 'Middlewaret',
+            tehtavia: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
 
     return (
         <div>
-            <Kurssi kurssi={kurssi} />
+            <h1>Opetetaan seuraavat kurssit</h1>
+            {kurssit.map(kurssi => <Kurssi kurssi={kurssi} key={kurssi.id}/>)}
+            
         </div>
     )
 }
