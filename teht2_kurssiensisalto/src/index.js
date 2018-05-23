ed6ec7679d294f3a932012d4f15fd930de14ce8e
learntopilk <jonsterivilja@gmail.com>
2018-05-23 21:17:40 +0300

    2.1 ja 2.2 tehty

---
+++ teht2_kurssiensisalto/src/index.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Yhteensa = (props) => {
+    let x = 0;
+    props.osat.forEach(element => {
+        x += element.tehtavia
+    });
+    return (
+        <div>
+            <p>Yhteensä {x} tehtävää</p>
+        </div>
+    )
+}
+const Kurssi = ({kurssi}) => {
+    return (
+        <div>
+            <Otsikko nimi={kurssi.nimi}/>
+            <Sisalto osat={kurssi.osat} />
+            <Yhteensa osat={kurssi.osat}/>
+        </div>
+    )
+}
+
+const Otsikko = ({nimi}) => {
+    return (
+        <h1>{nimi}</h1>
+    )
+}
+
+const Sisalto = ({osat}) => {
+    return (
+        <ul>
+            { osat.map(osa => <Osa osa={osa} key={osa.id}/>) }
+        </ul>
+    )
+}
+
+const Osa = ({osa}) => {
+    return (
+        <li>
+            <span>{osa.nimi}: </span><span>{osa.tehtavia} tehtävää</span>
+        </li>
+    )
+}
+
+const App = () => {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10,
+                id: 1
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7,
+                id: 2
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14,
+                id: 3
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Kurssi kurssi={kurssi} />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
