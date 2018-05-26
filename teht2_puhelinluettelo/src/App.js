e16f2bcf684c50e9d5f480d41a7fe3c3bd1ea51a
learntopilk <jonsterivilja@gmail.com>
2018-05-26 10:40:52 +0300

    Tehtävä 2.11 tehty!
    2.11 tehty, eli data haetaan axios-palvelulla ja käsitellään componentDidMount-metodissa.

--- teht2_puhelinluettelo/src/App.js
+++ teht2_puhelinluettelo/src/App.js
@@ -1,20 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NumberForm from './components/NumberForm'
+import Contact from './components/Contact'
+import axios from 'axios'
 
-class App extends Component {
+const Filter = ({ funk }) => {
+  return <div><span>Hakutermi: </span><input onChange={funk} /></div>
+}
+
+class App extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+          persons: [],
+          newName: '',
+          newNumber: '',
+          searchTerm: ''
+      }
+  }
+
+  componentDidMount() {
+      console.log("Starting fetch process")
+      axios
+          .get("http://localhost:3002/persons")
+          .then((response) => {
+              console.log("Dis be da promsies bulbilled", response.data)
+              this.setState({persons: response.data})
+          })
+  }
+
+  addEntry = (event) => {
+      event.preventDefault();
+      console.log(this.state.newName)
+
+      if (this.state.newName !== '') {
+
+          let arr = this.state.persons
+          if (!arr.some(x => x.name === this.state.newName)) {
+              arr.push({ name: this.state.newName, number: this.state.newNumber })
+              this.setState({ persons: arr, newName: '', newNumber: '' }, console.log("Another soul saved!", this.state.persons))
+          } else {
+              console.log("Contact already in store!")
+          }
+      }
+  }
+
+  onTextFieldChange = (event) => {
+      console.log(event.target.value)
+      this.setState({ newName: event.target.value })
+  }
+  onNumberFieldChange = (event) => {
+      console.log(event.target.value)
+      this.setState({ newNumber: event.target.value })
+  }
+
+  onFilterFieldChange = (event) => {
+      this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      const naytettavat =
+          this.state.searchTerm === '' ?
+              this.state.persons :
+              this.state.persons.filter(person => person.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+      return (
+          <div>
+              <Filter funk={this.onFilterFieldChange} />
+              <h2>Puhelinluettelo</h2>
+              <NumberForm thi={this} />
+
+              <div>debug: {this.state.newName}</div>
+              <h2>Numerot</h2>
+              {naytettavat.map(person => {
+                  return (
+                      <Contact person={person} key={person.name.concat(person.number)} />
+                  )
+              })}
+
+          </div>
+      )
   }
 }
 
